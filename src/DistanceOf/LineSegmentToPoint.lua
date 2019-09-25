80b2e9476dad572cdfa3544f2eccf9dbf2b52404
Zoe Judd <dog456pop@gmail.com>
2019-09-25 13:54:23 -0700

    Fix incorrect require path in LineSegmentToPoint

--- src/DistanceOf/LineSegmentToPoint.lua
+++ src/DistanceOf/LineSegmentToPoint.lua
@@ -1,7 +1,7 @@
 -- Reference: http://www.r-5.org/files/books/computers/algo-list/realtime-3d/Christer_Ericson-Real-Time_Collision_Detection-EN.pdf
 -- Page 129-130: Distance of Point to Segment
 
-local LineSegmentToPointSquared = require(script.ParentLineSegmentToPointSquared)
+local LineSegmentToPointSquared = require(script.Parent.LineSegmentToPointSquared)
 
 local function LineSegmentToPoint(segmentPointA, segmentPointB, pointC)
 	local distanceSquared = LineSegmentToPointSquared(segmentPointA, segmentPointB, pointC)
