b0c7fe1c4f1040c950309c260f55826e51459411
zoebasil <dog456pop@gmail.com>
2020-05-11 20:22:19 -0600

    Refactor line segment rectangle intersection into Utils, improve efficiency

--- src/ClosestPointsOf/LineSegmentAndRectangle.lua
+++ src/ClosestPointsOf/LineSegmentAndRectangle.lua
@@ -1,110 +1,6 @@
 local ClosestPointOnLineSegmentToPoint = require(script.Parent.Parent.ClosestPointOn.LineSegmentToPoint)
-local ClosestPointOnPlaneToPoint = require(script.Parent.Parent.ClosestPointOn.PlaneToPoint)
 local ClosestPointsOfLineSegments = require(script.Parent.LineSegments)
 local Utils = require(script.Parent.Parent.Utils)
-
--- Edge clamping functions: These are to calculate the point of intersection
--- between a rectangle edge and the 2d-projected line segment. Because of our
--- simplified axis-aligned representation of the 2d rectangle, we can
--- drastically simplify the calculations for these intersection points.
-
--- Y == 0 edge (AB)
-local function clampSegmentToEdgeAB(segA2dX, segA2dY, segB2dX, segB2dY, rectLength)
-	local xScalar = segA2dY / (segA2dY - segB2dY)
-	local x = segA2dX + xScalar * (segB2dX - segA2dX)
-
-	-- Clamp X between 0 and rectLength
-	if x < 0 then
-		return 0, 0, false
-	elseif x > rectLength then
-		return rectLength, 0, false
-	else
-		return x, 0, true
-	end
-end
--- Y == rectHeight edge (CD)
-local function clampSegmentToEdgeCD(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	local xScalar = (rectHeight - segA2dY) / (segB2dY - segA2dY)
-	local x = segA2dX + xScalar * (segB2dX - segA2dX)
-
-	-- Clamp X between 0 and rectLength
-	if x < 0 then
-		return 0, rectHeight, false
-	elseif x > rectLength then
-		return rectLength, rectHeight, false
-	else
-		return x, rectHeight, true
-	end
-end
--- X == 0 edge (DA)
-local function clampSegmentToEdgeAD(segA2dX, segA2dY, segB2dX, segB2dY, _, rectHeight)
-	local yScalar = segA2dX / (segA2dX - segB2dX)
-	local y = segA2dY + yScalar * (segB2dY - segA2dY)
-
-	-- Clamp Y between 0 and rectHeight
-	if y < 0 then
-		return 0, 0, false
-	elseif y > rectHeight then
-		return 0, rectHeight, false
-	else
-		return 0, y, true
-	end
-end
--- X == rectLength edge (BC)
-local function clampSegmentToEdgeBC(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	local yScalar = (rectLength - segA2dX) / (segB2dX - segA2dX)
-	local y = segA2dY + yScalar * (segB2dY - segA2dY)
-
-	-- Clamp Y between 0 and rectHeight
-	if y < 0 then
-		return rectLength, 0, false
-	elseif y > rectHeight then
-		return rectLength, rectHeight, false
-	else
-		return rectLength, y, true
-	end
-end
-
-local function clampSegmentToCorner(leftOrRightEdgeClampFunc, topOrBottomEdgeClampFunc, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	local clampedX, clampedY, didIntersect = leftOrRightEdgeClampFunc(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-
-	if didIntersect then
-		return clampedX, clampedY
-	end
-
-	return topOrBottomEdgeClampFunc(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-end
-
-local function getIntersectionForRectangleRegion2d(region, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	if region == 0 then
-		-- No intersection
-		return segA2dX, segA2dY
-	elseif region == 1 then
-		-- Segment intersects AB
-		return clampSegmentToEdgeAB(segA2dX, segA2dY, segB2dX, segB2dY, rectLength)
-	elseif region == 3 then
-		-- Segment intersects BC
-		return clampSegmentToEdgeBC(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	elseif region == 5 then
-		-- Segment intersects CD
-		return clampSegmentToEdgeCD(segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	elseif region == 7 then
-		-- Segment intersects AD
-		return clampSegmentToEdgeAD(segA2dX, segA2dY, segB2dX, segB2dY, nil, rectHeight)
-	elseif region == 2 then
-		-- Segment intersects either AB or BC
-		return clampSegmentToCorner(clampSegmentToEdgeBC, clampSegmentToEdgeAB, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	elseif region == 4 then
-		-- Segment intersects either BC or CD
-		return clampSegmentToCorner(clampSegmentToEdgeBC, clampSegmentToEdgeCD, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	elseif region == 6 then
-		-- Segment intersects either CD or AD
-		return clampSegmentToCorner(clampSegmentToEdgeAD, clampSegmentToEdgeCD, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	elseif region == 8 then
-		-- Segment intersects either AD or AB
-		return clampSegmentToCorner(clampSegmentToEdgeAD, clampSegmentToEdgeAB, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	end
-end
 
 local function clampSegmentPointsToRectangle(segA, segB, rectA, rectB, rectC, rectD)
 	local rectEdgeAB = rectB - rectA
@@ -113,30 +9,30 @@ local function clampSegmentPointsToRectangle(segA, segB, rectA, rectB, rectC, re
 	local rectangleXAxis = rectEdgeAB.Unit
 	local rectangleYAxis = rectEdgeAD.Unit
 
-	local rectLength = rectEdgeAB.Magnitude
-	local rectHeight = rectEdgeAD.Magnitude
+	local sizeX = rectEdgeAB.Magnitude
+	local sizeY = rectEdgeAD.Magnitude
 
 	local lineRectASegA = segA - rectA
 	local lineRectASegB = segB - rectA
 
-	local segA2dX = lineRectASegA:Dot(rectangleXAxis)
-	local segA2dY = lineRectASegA:Dot(rectangleYAxis)
+	local aX = lineRectASegA:Dot(rectangleXAxis)
+	local aY = lineRectASegA:Dot(rectangleYAxis)
 
-	local segB2dX = lineRectASegB:Dot(rectangleXAxis)
-	local segB2dY = lineRectASegB:Dot(rectangleYAxis)
+	local bX = lineRectASegB:Dot(rectangleXAxis)
+	local bY = lineRectASegB:Dot(rectangleYAxis)
 
-	local segARegion = Utils.getPlaneRegionFromRectangle(segA2dX, rectLength, segA2dY, rectHeight)
-	local segBRegion = Utils.getPlaneRegionFromRectangle(segB2dX, rectLength, segB2dY, rectHeight)
+	local segARegion = Utils.getPlaneRegionFromRectangle(aX, aY, sizeX, sizeY)
+	local segBRegion = Utils.getPlaneRegionFromRectangle(bX, bY, sizeX, sizeY)
 
 	if segBRegion < segARegion then
 		segARegion, segBRegion = segBRegion, segARegion
-		segA2dX, segA2dY, segB2dX, segB2dY = segB2dX, segB2dY, segA2dX, segA2dY
+		aX, aY, bX, bY = bX, bY, aX, aY
 	end
 
 	if segARegion == segBRegion then
 		if segARegion == 0 then
-			return rectA + rectangleXAxis * segA2dX + rectangleYAxis * segA2dY,
-				   rectA + rectangleXAxis * segB2dX + rectangleYAxis * segB2dY
+			return rectA + rectangleXAxis * aX + rectangleYAxis * aY,
+				   rectA + rectangleXAxis * bX + rectangleYAxis * bY
 		elseif segARegion == 8 then
 			return rectA
 		elseif segARegion == 2 then
@@ -158,11 +54,11 @@ local function clampSegmentPointsToRectangle(segA, segB, rectA, rectB, rectC, re
 		return rectD, rectA
 	end
 
-	local clampedA2dX, clampedA2dY = getIntersectionForRectangleRegion2d(segARegion, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
-	local clampedB2dX, clampedB2dY = getIntersectionForRectangleRegion2d(segBRegion, segA2dX, segA2dY, segB2dX, segB2dY, rectLength, rectHeight)
+	local aXClamped, aYClamped = Utils.getIntersectionForRectangle2dRaw(aX, aY, bX, bY, sizeX, sizeY)
+	local bXClamped, bYClamped = Utils.getIntersectionForRectangle2dRaw(aX, aY, bX, bY, sizeX, sizeY)
 
-	return rectA + rectangleXAxis * clampedA2dX +rectangleYAxis * clampedA2dY,
-		   rectA + rectangleXAxis * clampedB2dX + rectangleYAxis * clampedB2dY
+	return rectA + rectangleXAxis * aXClamped + rectangleYAxis * aYClamped,
+		   rectA + rectangleXAxis * bXClamped + rectangleYAxis * bYClamped
 end
 
 local function ClosestPointsOfLineSegmentAndRectangle(segA, segB, rectA, rectB, rectC, rectD)
--- src/Utils.lua
+++ src/Utils.lua
@@ -36,19 +36,19 @@ end
 
 -- Partitions
 -- https://imgur.com/a/S186utY
-function Utils.getPlaneRegionFromRectangle(x, rectLength, y, rectHeight)
+function Utils.getPlaneRegionFromRectangle(x, y, xSize, ySize)
 	if x < 0 then
 		if y < 0 then
 			return 8
-		elseif y < rectHeight then
+		elseif y < ySize then
 			return 7
 		else
 			return 6
 		end
-	elseif x < rectLength then
+	elseif x < xSize then
 		if y < 0 then
 			return 1
-		elseif y < rectHeight then
+		elseif y < ySize then
 			return 0
 		else
 			return 5
@@ -56,7 +56,7 @@ function Utils.getPlaneRegionFromRectangle(x, rectLength, y, rectHeight)
 	else
 		if y < 0 then
 			return 2
-		elseif y < rectHeight then
+		elseif y < ySize then
 			return 3
 		else
 			return 4
@@ -64,86 +64,100 @@ function Utils.getPlaneRegionFromRectangle(x, rectLength, y, rectHeight)
 	end
 end
 
-function Utils.getSpaceRegionFromRectangle(x, rectLength, y, rectHeight, rectDepth, z)
-	if y < 0 then
-		if x < 0 then
-			if z < 0 then
-				return 8
-			elseif z < rectDepth then
-				return 7
-			else
-				return 6
-			end
-		elseif x < rectLength then
-			if z < 0 then
-				return 1
-			elseif z < rectDepth then
-				return 9
-			else
-				return 5
-			end
-		else
-			if z < 0 then
-				return 2
-			elseif z < rectDepth then
-				return 3
-			else
-				return 4
-			end
-		end
-	elseif y < rectHeight then
-		if x < 0 then
-			if z < 0 then
-				return 18
-			elseif z < rectDepth then
-				return 17
-			else
-				return 16
-			end
-		elseif x < rectLength then
-			if z < 0 then
-				return 11
-			elseif z < rectDepth then
-				return 0
-			else
-				return 15
-			end
-		else
-			if z < 0 then
-				return 12
-			elseif z < rectDepth then
-				return 13
-			else
-				return 14
-			end
-		end
+-- Edge clamping functions: These are to calculate the point of intersection
+-- between a rectangle edge and a 2d line segment. Because of our
+-- simplified axis-aligned representation of the 2d rectangle, we can
+-- drastically simplify the calculations for these intersection points.
+
+-- Y == 0 edge (AB)
+local function clampSegmentToBottomEdge(aX, aY, bX, bY, xSize)
+	local xScalar = aY / (aY - bY)
+	local x = aX + xScalar * (bX - aX)
+
+	-- Clamp X between 0 and xSize
+	if x < 0 then
+		return 0, 0, false
+	elseif x > xSize then
+		return xSize, 0, false
 	else
-		if x < 0 then
-			if z < 0 then
-				return 28
-			elseif z < rectDepth then
-				return 27
-			else
-				return 26
-			end
-		elseif x < rectLength then
-			if z < 0 then
-				return 21
-			elseif z < rectDepth then
-				return 29
-			else
-				return 25
-			end
-		else
-			if z < 0 then
-				return 22
-			elseif z < rectDepth then
-				return 23
-			else
-				return 24
-			end
+		return x, 0, true
+	end
+end
+-- Y == ySize edge (CD)
+local function clampSegmentToTopEdge(aX, aY, bX, bY, xSize, ySize)
+	local xScalar = (ySize - aY) / (bY - aY)
+	local x = aX + xScalar * (bX - aX)
+
+	-- Clamp X between 0 and xSize
+	if x < 0 then
+		return 0, ySize, false
+	elseif x > xSize then
+		return xSize, ySize, false
+	else
+		return x, ySize, true
+	end
+end
+-- X == 0 edge (DA)
+local function clampSegmentToLeftEdge(aX, aY, bX, bY, _, ySize)
+	local yScalar = aX / (aX - bX)
+	local y = aY + yScalar * (bY - aY)
+
+	-- Clamp Y between 0 and ySize
+	if y < 0 then
+		return 0, 0, false
+	elseif y > ySize then
+		return 0, ySize, false
+	else
+		return 0, y, true
+	end
+end
+-- X == xSize edge (BC)
+local function clampSegmentToRightEdge(aX, aY, bX, bY, xSize, ySize)
+	local yScalar = (xSize - aX) / (bX - aX)
+	local y = aY + yScalar * (bY - aY)
+
+	-- Clamp Y between 0 and ySize
+	if y < 0 then
+		return xSize, 0, false
+	elseif y > ySize then
+		return xSize, ySize, false
+	else
+		return xSize, y, true
+	end
+end
+
+function Utils.getIntersectionForRectangle2dRaw(aX, aY, bX, bY, xSize, ySize)
+	if aX < 0 then
+		-- Line could intersect left edge.
+		local resultX, resultY, didIntersect = clampSegmentToLeftEdge(aX, aY, bX, bY, nil, ySize)
+
+		if didIntersect then
+			return resultX, resultY
+		end
+	elseif aX > xSize then
+		-- Line could intersect right edge.
+		local resultX, resultY, didIntersect = clampSegmentToRightEdge(aX, aY, bX, bY, xSize, ySize)
+
+		if didIntersect then
+			return resultX, resultY
 		end
 	end
+
+	if aY < 0 then
+		-- Line intersects bottom edge
+		return clampSegmentToBottomEdge(aX, aY, bX, bY, xSize)
+	elseif aY < ySize then
+		-- No intersection
+		return aX, aY
+	else
+		-- Line intersects top edge
+		return clampSegmentToTopEdge(aX, aY, bX, bY, xSize, ySize)
+	end
+end
+
+function Utils.getIntersectionForRectangle2d(aX, aY, bX, bY, xSize, ySize)
+	local x, y = Utils.getIntersectionForRectangle2dRaw(aX, aY, bX, bY, xSize, ySize)
+	return Vector2.new(x, y)
 end
 
 return Utils
